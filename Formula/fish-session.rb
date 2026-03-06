2e73d6413acb74a956126536b28e2691c0418233
Atef Ramadan <hi@ateframadan.me>
2026-03-06 05:48:26 +0200

    fish-session 0.1.5

--- Formula/fish-session.rb
+++ Formula/fish-session.rb
@@ -1,8 +1,8 @@
 class FishSession < Formula
   desc "UI-first session manager for fish shell"
   homepage "https://github.com/AtefR/fish-session"
-  url "https://github.com/AtefR/fish-session/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "5b5f5e72bef88bcddbf9fbfeae36194a3217051d6a1d5ebf0f2bf0bf27c445e3"
+  url "https://github.com/AtefR/fish-session/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "5275788c6619826c32113bd6389c67007f286d967fa30528b8e502c882f03a9c"
 
   depends_on "rust" => :build
 
